51c130bcc958e244cd03e649d69341d1aeea3d49
Leandro <leandrocgms1986@outlook.com>
2026-09-06 19:33:20 -0400

    Mensagem automática do script "enviar" em 06/09/2026, às 19:33:20

--- app/components/screens/HeaderComponent.js
+++ app/components/screens/HeaderComponent.js
@@ -2,9 +2,14 @@ import { useEffect, useRef, useState, useId } from "react";
 // import { GoogleReCaptchaProvider, useGoogleReCaptcha } from 'react-google-recaptcha-v3';
 import ReCaptchaProvider from './GoogleRecaptchaProvider'
 import { useRouter } from "next/navigation";
-
+import { BsMicrosoftTeams } from "react-icons/bs";
+import { SiZoom } from "react-icons/si";
+import { MdOutlineEmail } from "react-icons/md";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { FaGithub } from "react-icons/fa";
 
 export default function HeaderComponent() {
+    const classNamesButtonsContact = `cursor-pointer hover:bg-purple-700 active:bg-purple-600 p-2 rounded-lg bg-[white] border-[#CF27F5] border-1`;
     return (
         <div className="w-full flex flex-row text-white bg-black justify-between items-center p-2">
             <div >
@@ -17,10 +22,22 @@ export default function HeaderComponent() {
                     Área de Testes
                 </button>
             </div>
-            <div >
-                <button className="bg-gray-800 hover:bg-gray-700 active:bg-gray-600 text-white font-bold py-2 px-4 rounded-full cursor-pointer">
-                    Área de Testes
-                </button>
+            <div className="flex flex-row gap-4 cursor-pointer">
+                <BsMicrosoftTeams className={classNamesButtonsContact} size={40} color="#6264A7" onClick={() => {
+                    window.open(`https://teams.microsoft.com/l/chat/0/0?users=leandrocgms1986@outlook.com`, '_blank', 'noopener,noreferrer')
+                }} />
+                <SiZoom className={classNamesButtonsContact} size={40} color="#0000FF" onClick={() => {
+                    window.open(`https://zoom.us/start/videomeeting?email=leandrocgms2015@gmail.com`, '_blank', 'noopener,noreferrer')
+                }} />
+                <MdOutlineEmail className={`${classNamesButtonsContact} bg-black`} size={40} color="white" onClick={() => {
+                    window.open(`mailto:leandrocgms1986@outlook.com`, '_blank', 'noopener,noreferrer')
+                }} />
+                <IoLogoWhatsapp className={classNamesButtonsContact} size={40} color="green" onClick={() => {
+                    window.open(`https://wa.me/5567981712803`, '_blank', 'noopener,noreferrer')
+                }} />
+                <FaGithub className={`${classNamesButtonsContact} bg-black`} size={40} color="white" onClick={() => {
+                    window.open(`https://github.com/LeandroCGMS`, '_blank', 'noopener,noreferrer')
+                }} />
             </div>
         </div>
     )
