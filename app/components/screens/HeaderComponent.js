c037f1ff4fd3ff2a3480109ebd1fa56e249fa2ca
Leandro <leandrocgms1986@outlook.com>
2026-09-07 18:31:22 -0400

    Mensagem automática do script "enviar" em 07/09/2026, às 18:31:22

--- app/components/screens/HeaderComponent.js
+++ app/components/screens/HeaderComponent.js
@@ -11,7 +11,7 @@ import { FaGithub } from "react-icons/fa";
 export default function HeaderComponent() {
     const classNamesButtonsContact = `cursor-pointer hover:bg-gray-700 active:bg-gray-600 p-2 rounded-lg bg-[white] border-[#CF27F5] border-1`;
     return (
-        <div className="w-full flex flex-row text-white bg-black justify-between items-center p-2">
+        <div className="w-[100%] flex flex-row text-white bg-black justify-between items-center p-2 border border-white rounded-lg">
             <div >
                 <button className="bg-gray-800 hover:bg-gray-700 active:bg-gray-600 text-white font-bold py-2 px-4 rounded-full cursor-pointer">
                     Fale Conosco
@@ -23,7 +23,7 @@ export default function HeaderComponent() {
                 </button>
             </div>
             <div className="flex flex-row gap-4 justify-center items-center">
-                <h1 className={`bg-gray-800 hover:bg-gray-700 active:bg-gray-600 text-white font-bold py-2 px-4 rounded-full`}>Contatos: </h1>
+                <h1 className={`text-white bg-black font-mono`}>Contatos: </h1>
                 <BsMicrosoftTeams className={classNamesButtonsContact} size={40} color="#6264A7" onClick={() => {
                     window.open(`https://teams.microsoft.com/l/chat/0/0?users=leandrocgms1986@outlook.com`, '_blank', 'noopener,noreferrer')
                 }} />
