cc2910f6da2674754838311184047b080bc39b03
Leandro <leandrocgms1986@outlook.com>
2026-09-09 09:14:00 -0400

    Mensagem automática do script "enviar" em 09/09/2026, às 09:14:00

--- app/components/screens/HomeScreen.js
+++ app/components/screens/HomeScreen.js
@@ -92,9 +92,8 @@ export default function HomeScreen() {
                             height={150}
                         />
                     </div>
-                    {showLogoFullScreen && <div className={`cursor-pointer rounded-lg fixed inset-0 p-[2em] border-white border-[0.1em] flex flex-col items-center justify-center text-center bg-violet-700 z-30 overflow-scroll`} onClick={() => { setShowLogoFullScreen(false) }}>
-                        <h1 className={`text-black`}>Clique ou toque na tela para fechar essa visualização.</h1>
-                        <small>Se estiver no celular, melhor girar o celular, com giro de tela ativado, para exibir com largura maior.</small>
+                    {showLogoFullScreen && <div style={{ zIndex: 150 }} className={`cursor-pointer rounded-lg fixed inset-0 p-[2em] border-white border-[0.1em] flex flex-col items-center justify-center text-center bg-violet-700 z-30 overflow-scroll`} onClick={() => { setShowLogoFullScreen(false) }}>
+                        <h1 className={`text-white`}>Clique ou toque na tela para fechar essa visualização.</h1>
                         <div className={`w-[100%] h-[100%] relative justify-center items-center flex flex-col p-2`}>
                             <Image
                                 className={`relative rounded-lg m-2`}
@@ -105,9 +104,9 @@ export default function HomeScreen() {
                             />
                         </div>
                     </div>}
-                    <h1 className={`text-black bg-violet mt-1`}>Leandro Santos de Carvalho</h1>
-                    <h2 className={`text-black bg-violet`}>Corretor de Imóveis - CRECI/MS 18329</h2>
-                    <h2 className={`text-black bg-violet text-center`}>Programador Web e Mobile, Full Cycle</h2>
+                    <h1 className={`text-white bg-violet mt-1`}>Leandro Santos de Carvalho</h1>
+                    <h2 className={`text-white bg-violet`}>Corretor de Imóveis - CRECI/MS 18329</h2>
+                    <h2 className={`text-white bg-violet text-center`}>Programador Web e Mobile, Full Cycle</h2>
 
                     <button
                         onClick={() => {
